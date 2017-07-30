c1ab31a1a456bf2b50f586f6e132e8d94ea58086
Victor Domiciano <victor.dm3@gmail.com>
2017-07-30 09:20:27 -0300

    Fix elevator and powered door script

--- game/objects/powered_devices/powered_door.gd
+++ game/objects/powered_devices/powered_door.gd
@@ -1,6 +1,15 @@
 extends "res://objects/powered_devices/basic_powered_device.gd"
 
 onready var sprite = get_node('Sprite')
+onready var is_open = false
+onready var open_door = load("res://objects/powered_devices/metal_door_open.tex")
+onready var closed_door = load("res://objects/powered_devices/metal_door_closed.tex")
+
 func interact(character):
 	if (power_cell.get_power_cell_energy() > 0):
-		print('enter elevator')
+		if (!is_open):
+			sprite.set_texture(open_door)
+			is_open = true
+		else:
+			sprite.set_texture(closed_door)
+			is_open = false
