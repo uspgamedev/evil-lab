8b5f664ebf09a63ff987645ba05bc75f08f3d688
Kazuo256 <kazuo256@gmail.com>
2017-07-31 15:05:37 -0300

    Lock message

--- game/objects/door/door.gd
+++ game/objects/door/door.gd
@@ -2,8 +2,16 @@ extends StaticBody2D
 
 export(String) var target_room_name
 export(String) var spawn_tag
+export(bool) var locked = false
 
 signal exit_room(target_room_name, spawn_tag)
+signal exit_failed
+
+func unlock():
+	locked = false
 
 func interact(character):
-	emit_signal("exit_room", self.target_room_name, self.spawn_tag)
+	if locked:
+		emit_signal("exit_failed")
+	else:
+		emit_signal("exit_room", self.target_room_name, self.spawn_tag)
