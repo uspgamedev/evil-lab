e0cfee96c297d111cae6f83166e39d5111c2a367
orenjiakira <orenjiakira@gmail.com>
2017-07-31 21:16:05 -0300

    Make door make a sound when opened

--- game/objects/door/door.gd
+++ game/objects/door/door.gd
@@ -4,6 +4,8 @@ export(String) var target_room_name
 export(String) var spawn_tag
 export(bool) var locked = false
 
+onready var sfx = get_node("/root/Main/door_sfx")
+
 signal exit_room(target_room_name, spawn_tag)
 signal exit_failed
 
@@ -14,4 +16,5 @@ func interact(character):
 	if locked:
 		emit_signal("exit_failed")
 	else:
+		sfx.go()
 		emit_signal("exit_room", self.target_room_name, self.spawn_tag)
